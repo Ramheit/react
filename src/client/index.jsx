19ab89b64be3149ab02991f7423c0169018d11fe
Ramheit <dekhtyar.is@yandex.ru>
2020-12-02 09:46:10 +0300

    Первый фикс, остановка на уроке 2.2

---
+++ src/client/index.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Header } from '../shared/header';
+
+window.addEventListener('load', () => {
+    ReactDOM.render(<Header />, document.getElementById('root'));
+});
---
+++ src/shared/header.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export function Header(){
+    return(
+        <header>
+            <h1>My React Sample =3</h1>
+        </header>    
+    );
+}
